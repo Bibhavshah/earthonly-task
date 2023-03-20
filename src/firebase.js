9c600254b544ec4dbb58983888b478b87e94b62e
Bibhavshah <78656387+Bibhavshah@users.noreply.github.com>
2023-03-20 13:57:43 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCUZuc5X8f8jsmRjFyevDFb_aDX4ci7sSg",
-  authDomain: "heroku-mock-app.firebaseapp.com",
-  projectId: "heroku-mock-app",
-  storageBucket: "heroku-mock-app.appspot.com",
-  messagingSenderId: "675756311069",
-  appId: "1:675756311069:web:bb1e7a7a10a347a33d7cf4",
-  measurementId: "G-YK7QW5ERM4"
+  apiKey: 'AIzaSyCUZuc5X8f8jsmRjFyevDFb_aDX4ci7sSg',
+  authDomain: 'heroku-mock-app.firebaseapp.com',
+  projectId: 'heroku-mock-app',
+  storageBucket: 'heroku-mock-app.appspot.com',
+  messagingSenderId: '675756311069',
+  appId: '1:675756311069:web:bb1e7a7a10a347a33d7cf4',
+  measurementId: 'G-YK7QW5ERM4'
 };
 
 const app = initializeApp(firebaseConfig);
